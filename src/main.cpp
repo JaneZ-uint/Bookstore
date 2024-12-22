d87bf21a4e97f7f1dec1be3224f2c3d2b347ada0
gyhyz917 <zhuyihan0826@outlook.com>
2024-12-22 19:30:24 +0800

    Finish Error.cpp & Error.h & Part of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,30 @@
 #include "../include/Tokenscanner.h"
 #include "../include/User.h"
 
+bool processline(std::string line , Book bookmanage , User usermanage , Finance financemanage , Blog blogmanage);
+
 int main() {
     Book BookManage; //管理图书信息及相关操作的类
-
+    User UserManage; //管理账户系统
+    Finance FinanceManage; //管理财务系统
+    Blog BlogManage; //管理日志系统
+    std::string command;
+    while(true) {
+        try {
+            if(!getline(std::cin , command)) {
+                command = "quit";
+            }
+            if(!processline(command , BookManage , UserManage , FinanceManage,BlogManage)) {
+                break;
+            }
+        }catch(InvalidExpression &error) {
+            std::cout << error.what();
+        }
+    }
 
     return 0;
+}
+
+bool processline(std::string line,Book bookmanage,User usermanage,Finance financemanage,Blog blogmanage) {
+
 }
