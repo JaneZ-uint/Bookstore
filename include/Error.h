2a3ff1be981ced9ab786c70234dda9c7a4bd03d2
gyhyz917 <zhuyihan0826@outlook.com>
2024-12-22 19:28:19 +0800

    Finish Error.cpp & Error.h

--- include/Error.h
+++ include/Error.h
@@ -4,5 +4,12 @@
 
 #ifndef ERROR_H
 #define ERROR_H
+#include <exception>
+#include <string>
+
+class InvalidExpression :public std::exception {
+public:
+    const char* what() const noexcept override;
+};
 
 #endif //ERROR_H
--- src/Error.cpp
+++ src/Error.cpp
@@ -1,3 +1,7 @@
 //
 // Created by zhuyi on 2024/12/20.
 //
+#include "../include/Error.h"
+ const char* InvalidExpression::what() const noexcept {
+    return "Invalid\n";
+ }
