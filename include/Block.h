c2393fbfdfbd54249ca89ddd90d057a5f8d9a0a3
gyhyz917 <zhuyihan0826@outlook.com>
2024-12-24 18:18:50 +0800

    Still change Block.h to template (cuz I found a problem...

--- include/Block.h
+++ include/Block.h
@@ -1,7 +1,3 @@
-//
-// Created by zhuyi on 2024/12/20.
-//
-
 #ifndef BLOCK_H
 #define BLOCK_H
 #include <iostream>
@@ -13,17 +9,16 @@
 
 const int Block_size = 500;  // 块长可能需要调整
 const int Key_size = 71; // 键的长度固定为 71 字节
-const int Value_size =  71 ;// 值的长度固定为 71 字节
 
 // 定义模板参数
-template <int BlockSize = Block_size, int KeySize = Key_size, int ValueSize = Value_size>
+template <int BlockSize = Block_size, int KeySize = Key_size, typename ValueType>
 class Block {
 private:
-    using arr = std::array<std::pair<char[KeySize + 1], char[ValueSize + 1]>, BlockSize>; // 键为 char[KeySize], 值为 char[ValueSize]
+    using arr = std::array<std::pair<char[KeySize + 1], ValueType>, BlockSize>; // 键为 char[KeySize], 值为 ValueType
 
     struct head {
-        std::pair<char[KeySize + 1], char[ValueSize + 1]> maxele;
-        std::pair<char[KeySize + 1], char[ValueSize + 1]> minele;
+        std::pair<char[KeySize + 1], ValueType> maxele;
+        std::pair<char[KeySize + 1], ValueType> minele;
         int size = 0;
         int indexnum = 0;
     };
@@ -41,63 +36,15 @@ private:
     node* Head;
     node* tair;
 
-    // 比较函数
-    bool larger(const std::pair<char[KeySize + 1], char[ValueSize + 1]>& a, const std::pair<char[KeySize + 1], char[ValueSize + 1]>& b) {
-        int cmp = strcmp(a.first, b.first);
-        if (cmp > 0) {
-            return true;
-        } else if (cmp < 0) {
-            return false;
-        } else {
-            return strcmp(a.second, b.second) > 0;
-        }
-    }
-
-    bool largerequal(const std::pair<char[KeySize + 1], char[ValueSize + 1]>& a, const std::pair<char[KeySize + 1], char[ValueSize + 1]>& b) {
-        int cmp = strcmp(a.first, b.first);
-        if (cmp > 0) {
-            return true;
-        } else if (cmp == 0) {
-            return strcmp(a.second, b.second) >= 0;
-        }
-        return false;
-    }
-
-    bool smaller(const std::pair<char[KeySize + 1], char[ValueSize + 1]>& a, const std::pair<char[KeySize + 1], char[ValueSize + 1]>& b) {
-        int cmp = strcmp(a.first, b.first);
-        if (cmp < 0) {
-            return true;
-        } else if (cmp > 0) {
-            return false;
-        } else {
-            return strcmp(a.second, b.second) < 0;
-        }
-    }
-
-    bool smallerequal(const std::pair<char[KeySize + 1], char[ValueSize + 1]>& a, const std::pair<char[KeySize + 1], char[ValueSize + 1]>& b) {
-        int cmp = strcmp(a.first, b.first);
-        if (cmp < 0) {
-            return true;
-        } else if (cmp == 0) {
-            return strcmp(a.second, b.second) <= 0;
-        }
-        return false;
-    }
-
-    bool equal(const std::pair<char[KeySize + 1], char[ValueSize + 1]>& a, const std::pair<char[KeySize + 1], char[ValueSize + 1]>& b) {
-        return strcmp(a.first, b.first) == 0 && strcmp(a.second, b.second) == 0;
-    }
-
     // 分块函数
-    void splitBlock(node* current, const std::pair<char[KeySize + 1], char[ValueSize + 1]>& tmp, arr& a, int i) {
+    void splitBlock(node* current, const std::pair<char[KeySize + 1], ValueType>& tmp, arr& a, int i) {
         head new_headnode; // 创建新的头节点
         arr a0;
         if (i == BlockSize) {
             // 说明来到了整个块状链表的最后一个位置
             strncpy(a0[0].first, tmp.first, KeySize);
             a0[0].first[KeySize] = '\0'; // 确保字符串以 '\0' 结尾
-            strncpy(a0[0].second, tmp.second, ValueSize);
-            a0[0].second[ValueSize] = '\0';
+            a0[0].second = tmp.second;
             new_headnode.size = 1;
             file2.seekp(0, std::ios::end);
         } else {
@@ -105,19 +52,15 @@ private:
             for (int j = i; j < current->info.size; j++) {
                 strncpy(a0[j - i].first, a[j].first, KeySize);
                 a0[j - i].first[KeySize] = '\0';
-                strncpy(a0[j - i].second, a[j].second, ValueSize);
-                a0[j - i].second[ValueSize] = '\0';
+                a0[j - i].second = a[j].second;
             }
             for (int j = i; j < current->info.size; j++) {
                 strncpy(a[j].first, "\0", KeySize);
                 a[j].first[KeySize] = '\0';
-                strncpy(a[j].second, "\0", ValueSize);
-                a[j].second[ValueSize] = '\0';
             }
             strncpy(a[i].first, tmp.first, KeySize);
             a[i].first[KeySize] = '\0';
-            strncpy(a[i].second, tmp.second, ValueSize);
-            a[i].second[ValueSize] = '\0';
+            a[i].second = tmp.second;
             current->info.size = i + 1;
             file2.seekp(current->info.indexnum * sizeof(arr));
             file2.write(reinterpret_cast<char*>(&a), sizeof(a));
@@ -130,20 +73,16 @@ private:
         current->info.size = std::min(i + 1, BlockSize);
         strncpy(current->info.minele.first, a[0].first, KeySize);
         current->info.minele.first[KeySize] = '\0';
-        strncpy(current->info.minele.second, a[0].second, ValueSize);
-        current->info.minele.second[ValueSize] = '\0';
+        current->info.minele.second = a[0].second;
         strncpy(current->info.maxele.first, a[current->info.size - 1].first, KeySize);
         current->info.maxele.first[KeySize] = '\0';
-        strncpy(current->info.maxele.second, a[current->info.size - 1].second, ValueSize);
-        current->info.maxele.second[ValueSize] = '\0';
+        current->info.maxele.second = a[current->info.size - 1].second;
         strncpy(new_headnode.minele.first, a0[0].first, KeySize);
         new_headnode.minele.first[KeySize] = '\0';
-        strncpy(new_headnode.minele.second, a0[0].second, ValueSize);
-        new_headnode.minele.second[ValueSize] = '\0';
+        new_headnode.minele.second = a0[0].second;
         strncpy(new_headnode.maxele.first, a0[new_headnode.size - 1].first, KeySize);
         new_headnode.maxele.first[KeySize] = '\0';
-        strncpy(new_headnode.maxele.second, a0[new_headnode.size - 1].second, ValueSize);
-        new_headnode.maxele.second[ValueSize] = '\0';
+        new_headnode.maxele.second = a0[new_headnode.size - 1].second;
 
         node* p = new node(new_headnode);
         p->next = current->next;
@@ -190,14 +129,14 @@ public:
         }
     }
 
-    void insert(const char* key, const char* value) {
+    // insert操作
+    bool insert(const char* key, const ValueType& value) {
         file2.open(file2_name, std::ios::in | std::ios::out | std::ios::binary);
         node* current = Head->next;
-        std::pair<char[KeySize + 1], char[ValueSize + 1]> tmp;
+        std::pair<char[KeySize + 1], ValueType> tmp;
         strncpy(tmp.first, key, KeySize);
         tmp.first[KeySize] = '\0'; // 确保字符串以 '\0' 结尾
-        strncpy(tmp.second, value, ValueSize);
-        tmp.second[ValueSize] = '\0';
+        tmp.second = value;
         arr a;
         while (current != nullptr) {
             if (tmp <= current->info.maxele || current->next == nullptr) {
@@ -212,30 +151,26 @@ public:
                 }
                 if (a[i] == tmp) {
                     file2.close();
-                    return;
+                    return false; // 键已经存在，插入失败
                 } else {
                     // 如果块还未满
                     if (current->info.size < BlockSize) {
                         for (int j = current->info.size - 1; j >= i; j--) {
                             strncpy(a[j + 1].first, a[j].first, KeySize);
                             a[j + 1].first[KeySize] = '\0';
-                            strncpy(a[j + 1].second, a[j].second, ValueSize);
-                            a[j + 1].second[ValueSize] = '\0';
+                            a[j + 1].second = a[j].second;
                         }
                         strncpy(a[i].first, key, KeySize);
                         a[i].first[KeySize] = '\0';
-                        strncpy(a[i].second, value, ValueSize);
-                        a[i].second[ValueSize] = '\0';
+                        a[i].second = value;
                         file2.seekp(current->info.indexnum * sizeof(arr));
                         file2.write(reinterpret_cast<char*>(&a), sizeof(arr));
                         strncpy(current->info.minele.first, a[0].first, KeySize);
                         current->info.minele.first[KeySize] = '\0';
-                        strncpy(current->info.minele.second, a[0].second, ValueSize);
-                        current->info.minele.second[ValueSize] = '\0';
+                        current->info.minele.second = a[0].second;
                         strncpy(current->info.maxele.first, a[current->info.size].first, KeySize);
                         current->info.maxele.first[KeySize] = '\0';
-                        strncpy(current->info.maxele.second, a[current->info.size].second, ValueSize);
-                        current->info.maxele.second[ValueSize] = '\0';
+                        current->info.maxele.second = a[current->info.size].second;
                         current->info.size++;
                     } else {
                         // 如果块已经满了，进行裂块操作
@@ -243,47 +178,44 @@ public:
                     }
                 }
                 file2.close();
-                return;
+                return true; // 插入成功
             }
             current = current->next;
         }
         strncpy(a[0].first, tmp.first, KeySize);
         a[0].first[KeySize] = '\0';
-        strncpy(a[0].second, tmp.second, ValueSize);
-        a[0].second[ValueSize] = '\0';
+        a[0].second = tmp.second;
         file2.seekp(0);
         file2.write(reinterpret_cast<char*>(&a), sizeof(arr));
         head n;
         strncpy(n.maxele.first, tmp.first, KeySize);
         n.maxele.first[KeySize] = '\0';
-        strncpy(n.maxele.second, tmp.second, ValueSize);
-        n.maxele.second[ValueSize] = '\0';
+        n.maxele.second = tmp.second;
         strncpy(n.minele.first, tmp.first, KeySize);
         n.minele.first[KeySize] = '\0';
-        strncpy(n.minele.second, tmp.second, ValueSize);
-        n.minele.second[ValueSize] = '\0';
+        n.minele.second = tmp.second;
         n.size = 1;
         tair = new node(n);
         Head->next = tair;
         file2.close();
+        return true; // 插入成功
     }
 
-    // delete操作
-    void remove(const char* key, const char* value) {
+    // remove操作
+    bool remove(const char* key, const ValueType& value) {
         file2.open(file2_name, std::ios::in | std::ios::out | std::ios::binary);
         node* current = Head;
-        std::pair<char[KeySize + 1], char[ValueSize + 1]> tmp;
+        std::pair<char[KeySize + 1], ValueType> tmp;
         strncpy(tmp.first, key, KeySize);
         tmp.first[KeySize] = '\0';
-        strncpy(tmp.second, value, ValueSize);
-        tmp.second[ValueSize] = '\0';
+        tmp.second = value;
         while (current->next != nullptr) {
             // 如果在这个块中
             if (current->next->info.minele <= tmp && current->next->info.maxele >= tmp) {
                 arr a;
                 file2.seekg(current->next->info.indexnum * sizeof(arr));
                 file2.read(reinterpret_cast<char*>(&a), sizeof(arr));
-                for (int i = 0; i <= current->next->info.size; i++) {
+                for (int i = 0; i < current->next->info.size; i++) {
                     if (a[i] == tmp) {
                         // 如果当前块中只有一个元素 需要删除空块
                         if (current->next->info.size == 1) {
@@ -294,50 +226,47 @@ public:
                             for (int j = i + 1; j < current->next->info.size; j++) {
                                 strncpy(a[j - 1].first, a[j].first, KeySize);
                                 a[j - 1].first[KeySize] = '\0';
-                                strncpy(a[j - 1].second, a[j].second, ValueSize);
-                                a[j - 1].second[ValueSize] = '\0';
+                                a[j - 1].second = a[j].second;
                             }
                             current->next->info.size--;
                             // 更新最小元素
                             strncpy(current->next->info.minele.first, a[0].first, KeySize);
                             current->next->info.minele.first[KeySize] = '\0';
-                            strncpy(current->next->info.minele.second, a[0].second, ValueSize);
-                            current->next->info.minele.second[ValueSize] = '\0';
+                            current->next->info.minele.second = a[0].second;
                             // 更新最大元素
                             strncpy(current->next->info.maxele.first, a[current->next->info.size - 1].first, KeySize);
                             current->next->info.maxele.first[KeySize] = '\0';
-                            strncpy(current->next->info.maxele.second, a[current->next->info.size - 1].second, ValueSize);
-                            current->next->info.maxele.second[ValueSize] = '\0';
+                            current->next->info.maxele.second = a[current->next->info.size - 1].second;
                             // 尾置元素 清零
                             strncpy(a[current->next->info.size].first, "\0", KeySize);
                             a[current->next->info.size].first[KeySize] = '\0';
-                            strncpy(a[current->next->info.size].second, "\0", ValueSize);
-                            a[current->next->info.size].second[ValueSize] = '\0';
                             file2.seekp(current->next->info.indexnum * sizeof(arr));
                             file2.write(reinterpret_cast<char*>(&a), sizeof(arr));
                         }
                         file2.close();
-                        return;
+                        return true; // 删除成功
                     }
                     if (larger(a[i], tmp)) {
                         file2.close();
-                        return;
+                        return false; // 未找到，删除失败
                     }
                 }
             }
             current = current->next;
         }
         file2.close();
+        return false; // 未找到，删除失败
     }
 
     // find操作
-    void find(const char* key) {
+    bool find(const char* key) {
         file2.open(file2_name, std::ios::binary | std::ios::in | std::ios::out);
         node* current = Head->next;
         char tmp[KeySize + 1] = {'\0'};
         strncpy(tmp, key, KeySize);
         tmp[KeySize] = '\0';
-        std::vector<std::string> result;
+        std::vector<ValueType> result;
+        bool found = false;
         while (current != nullptr) {
             // 如果有在当前块中的可能性
             if (strcmp(current->info.minele.first, tmp) <= 0 && strcmp(current->info.maxele.first, tmp) >= 0) {
@@ -347,6 +276,7 @@ public:
                 for (int i = 0; i < current->info.size; i++) {
                     if (strcmp(a[i].first, tmp) == 0) {
                         result.push_back(a[i].second);
+                        found = true;
                     } else if (strcmp(a[i].first, tmp) > 0) {
                         break;
                     }
@@ -356,17 +286,9 @@ public:
             }
             current = current->next;
         }
-        if (result.empty()) {
-            std::cout << "null";
-        } else {
-            std::sort(result.begin(), result.end());
-            for (auto it : result) {
-                std::cout << it << " ";
-            }
-        }
-        std::cout << std::endl;
         file2.close();
+        return found; // 返回是否找到
     }
 };
 
-#endif //BLOCK_H
+#endif // BLOCK_H
